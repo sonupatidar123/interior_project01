80687529f33cee25d85991e05b30c654f5ebd582
sonupatidar123 <sonupatidar5888@gmail.com>
2026-02-05 15:18:08 +0530

    Fixed admin template error and static root

--- interior_project/settings.py
+++ interior_project/settings.py
@@ -73,9 +73,10 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
+        'APP_DIRS': True, # 👈 Ye True hona zaroori hai
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug', # 👈 Ye line add karein
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -83,7 +84,6 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = 'interior_project.wsgi.application'
 
 
@@ -136,7 +136,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
